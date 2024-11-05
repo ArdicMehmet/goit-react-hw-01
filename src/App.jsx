05247a411897d293631a6e192ad308da081815fd
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-11-05 15:40:00 +0300

    added json file for user

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
 import "./App.css";
 import Profile from "./components/Profile";
+import userData from "../userData.json";
 
 function App() {
-  const [user, setUser] = useState({
-    name: "Mehmet Ardıç",
-    tag: "ardicmehmet96",
-    location: {
-      city: "Eskişehir",
-      country: "Turkey",
-    },
-    image: "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
-    stats: [
-      { name: "Followers", content: "5603" },
-      { name: "Views", content: "4827" },
-      { name: "Likes", content: "1308" },
-    ],
-  });
   return (
     <>
       <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        image={user.image}
-        stats={user.stats}
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
       />
     </>
   );
