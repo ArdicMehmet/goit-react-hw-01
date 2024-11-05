c3b9012193db9fa7652d1aa746171f960bb80201
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-11-05 11:37:56 +0300

    created avatar image component

--- src/components/AvatarImage/index.jsx
+++ src/components/AvatarImage/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-
+import styles from "./avatarImage.module.css";
 const AvatarImage = () => {
-  return <div>AvatarImage</div>;
+  return (
+    <img
+      className={`${styles.avatarImage}`}
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="User avatar"
+    />
+  );
 };
 export default AvatarImage;
