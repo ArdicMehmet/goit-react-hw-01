8fabb8a8d9f452133273968027c1b0928c4442b1
Mehmet Ardic <ardicmehmet96@gmail.com>
2024-11-06 20:32:45 +0300

    changed variable name

--- src/components/Transaction/TableRow/index.jsx
+++ src/components/Transaction/TableRow/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./tableRow.module.css";
 const TableRow = ({ type, amount, currency, rowType }) => {
-  const row =
+  const classType =
     rowType == "Head" ? styles.headContainer : styles.transactionListItem;
   return (
-    <tr className={`${row} row`}>
+    <tr className={`${classType} row`}>
       <td className={`${styles.column}`}>{type}</td>
       <td className={`${styles.column}`}>{amount}</td>
       <td className={`${styles.column}`}>{currency}</td>
